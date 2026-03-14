4a087ab512d354771aa9123de0e3544747123d37
cecil185 <cash@teamworks.com>
2026-03-14 07:03:31 -0400

    c

--- cli.py
+++ cli.py
@@ -14,6 +14,7 @@ from src.workflow import Workflow
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(name)s: %(message)s")
 logger = logging.getLogger(__name__)
 
+
 app = typer.Typer()
 
 
